887d93a32a72c46c09d96c220d2d4750b6faf4f6
Trippie29 <pirrimontekinto29@gmail.com>
2026-04-25 12:40:39 +0200

    Add files via upload

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -63,8 +63,7 @@ const getSheetsToken = async () => {
   const pemContents = SHEETS_PRIVATE_KEY
     .replace('-----BEGIN PRIVATE KEY-----', '')
     .replace('-----END PRIVATE KEY-----', '')
-    .replace(/
-/g, '')
+    .split('\n').join('')
   
   const binaryDer = Uint8Array.from(atob(pemContents), c => c.charCodeAt(0))
   const cryptoKey = await crypto.subtle.importKey(
